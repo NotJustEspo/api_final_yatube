54ed9927d8bb4220acbb7f492887a7fb7431d653
Espo <notjustespo@yandex.ru>
2023-12-06 11:58:31 +0300

    Added djoser + jwt in urls.py and settings

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -5,10 +5,11 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.jwt')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
 ]
-
